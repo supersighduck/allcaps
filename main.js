ff93c3cd2d7241feb69fbf16b147891173b86430
supersighduck <99636798+supersighduck@users.noreply.github.com>
2022-03-19 04:24:12 -0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('upperCaseButton').addEventListener('click', buttonClick)
-
-function buttonClick() {
-    let userInfo = document.getElementById('upperCaseInfo').value
-    let convertMsg = userInfo.toUpperCase()
-    document.getElementById('upperCaseDisplay').innerText = convertMsg
-    document.getElementById('resetForm').reset()
-}
